3a5bd89805a2c81f208c53931db2978dd523435d
Emre Yalcin <muhammedemreyalcin2000@live.de>
2024-05-25 13:07:04 +0300

    web app continues

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -3,14 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 from .models import CustomUser as User
 
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female')
+]
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
 
     class Meta:
         model = User
-        fields = ['username', 'email',
-                  'orig_name', 'password1', 'password2']
+        fields = ['username', 'email', 'gender', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
